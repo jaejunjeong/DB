2476a05841d140ecafc93c1dc1f016d80d670690
Jaejun <92967394+jaejunjeong@users.noreply.github.com>
2023-04-19 23:10:08 +0900

    Update PostgreSQL.sql

--- Materialized_Views/PostgreSQL.sql
+++ Materialized_Views/PostgreSQL.sql
@@ -4,3 +4,9 @@ CREATE MATERIALIZED VIEW MY_MAT_VIEW
   AS SELECT * FROM <table_name> ;
   
   REFRESH MATERIALIZED VIEW MY_MAT_VIEW
+
+#Example
+CREATE MATERIALIZED VIEW avg_customer_bill (customerid, averagebillamount) 
+  AS SELECT customerid, avg(billedamount) FROM public."FactBilling" 
+  group by customerid ;
+  REFRESH MATERIALIZED VIEW avg_customer_bill;
